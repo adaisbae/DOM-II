7ed42ab70c546f277bcfecbc7ba757d40907767e
adaisbae <melissajoneswrites@gmail.com>
2019-06-18 17:16:34 -0400

    changed signup buttons background color

--- js/index.js
+++ js/index.js
@@ -1,17 +1,30 @@
 // Your code goes here
-let logo = document.getElementsByClassName('logo-heading')[0];
-let headerImage = document.querySelector('.intro img');
-let signUpButton = document.getElementsByClassName('.btn');
-
+let logo = document.getElementsByClassName('logo-heading');
+let images = document.querySelectorAll('img');
+let signUpButton = document.getElementsByClassName('btn');
+let destinationText = document.querySelector('.content-destination h2');
 
 // Callback Functions
 
 // Change the font color
-function fontColor(event){
+function blueFont(event){
     event.target.style.color = 'blue';
 }
+
+function pinkBackground(event){
+    event.target.style.background = 'pink';    
+  };
+
 
 
 // Event Listeners
 
-logo.addEventListener('mouseover', fontColor);
+// mouseover
+logo[0].addEventListener('mouseover', blueFont);
+
+// click
+for (i = 0; i < signUpButton.length; i++){
+signUpButton[i].addEventListener('click', pinkBackground)};
+
+// keydown
+
