bb2be69268f6256957d91ec7b1d8662e60046eed
adaisbae <melissajoneswrites@gmail.com>
2019-06-18 16:13:05 -0400

    changed logo color on mouseover

--- js/index.js
+++ js/index.js
@@ -1,8 +1,17 @@
 // Your code goes here
-let logo = document.getElementsByClassName('logo-heading');
+let logo = document.getElementsByClassName('logo-heading')[0];
 let headerImage = document.querySelector('.intro img');
 let signUpButton = document.getElementsByClassName('.btn');
 
 
-// 
+// Callback Functions
 
+// Change the font color
+function fontColor(event){
+    event.target.style.color = 'blue';
+}
+
+
+// Event Listeners
+
+logo.addEventListener('mouseover', fontColor);
