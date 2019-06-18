bfc2517e8ec341b3dd1a5b09fe4269ffa440b2ad
adaisbae <melissajoneswrites@gmail.com>
2019-06-18 15:27:56 -0400

    created selectors for event listeners

--- js/index.js
+++ js/index.js
@@ -1,1 +1,8 @@
 // Your code goes here
+let logo = document.getElementsByClassName('logo-heading');
+let headerImage = document.querySelector('.intro img');
+let signUpButton = document.getElementsByClassName('.btn');
+
+
+// 
+
